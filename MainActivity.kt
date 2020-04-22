436b214cd89628d77c90f6513f57f645f2ffd249
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2020-04-23 03:15:41 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinfun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var textView = findViewById<TextView>(R.id.textView)
+        textView.setText("Hello Avinash!!")
+    }
+}
